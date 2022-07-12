48d3f32a6e29544e05fb1dde7782b660be0a4726
psandeepaxioned <vikasb@axioned.com>
2022-07-12 19:29:31 +0530

    codepush on master

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,60 @@
-/* Author: 
+	// /* Author: 
 
-*/
+	// */
+	// const background = document.querySelector(".slider");
+	// console.log(background)
+	// const slides = document.querySelectorAll(".slide");
 
+	// const leftBtn = document.querySelector(".left");
+	// const rightBtn = document.querySelector(".right");
+	// const activeClass = document.querySelector(".active img");
+	// const bgImageSrc = document.querySelector(".slider-background");
 
+	// slides.innerText = '';
+	// let imageIndex = 0;
 
+	// rightBtn.addEventListener("click", () => {
+	//   imageIndex++;
 
+	//   if (imageIndex > slides.length - 1) {
+	//     imageIndex = 0;
+	//   }
 
+	//   setBackground();
+	//   addActiveClass();
+	// });
 
+	// leftBtn.addEventListener("click", () => {
+	//   imageIndex--;
 
+	//   if (imageIndex < 0) {
+	//     imageIndex = slides.length - 1;
+	//   }
 
+	//   setBackground();
+	//   addActiveClass();
+	// });
 
+	// console.log(setBackground);
+	// function addActiveClass() {
+		
+	//   slides.forEach((slide) => slide.classList.remove("active"));
 
+	//   slides[imageIndex].classList.add("active");
+	// }
 
+	// setBackground();
 
-
-
-
-
-
-
-
-
-
-
-
+	// function setBackground() {
+	// 	background.style.backgroundImage = slides[imageIndex].style.backgroundImage
+		
+	//   // // const target = activeClass.getAttribute("src");
+	//   // // console.log(target);
+	//   // // const source = `
+	//   // //   <figure>
+	//   // //       <img src="${target}" alt="background Image">
+	//   // //   </figure>
+	//   // //   `;
+	//   // bgImageSrc.innerHTML = source;
+	//   // console.log(bgImageSrc.innerHTML);
+	// }
